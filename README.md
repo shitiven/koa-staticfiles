342f91e2715d960bfdb3b8a6ded375e3cb85830e
fackweb@gmail.com <shitiven@Shitiven.local>
2017-01-10 15:38:54 +0800

    Initial commit

--- README.md
+++ README.md
@@ -1,2 +1,56 @@
 # koa-staticfiles
 Static file serving middleware for koa 2.x with directory, rewrite and index support
+
+## Installation
+
+```bash
+$ npm install koa-staticfiles
+```
+
+## API
+
+```js
+var koa = require('koa');
+var app = koa();
+app.use(require('koa-staticfiles')(rootDir, opts));
+```
+
+* `rootDir` root directory string. nothing above this root directory can be served
+* `opts` options object.
+
+### Options
+
+- `prefx` rewrite path
+ - `maxage` Browser cache max-age in milliseconds. defaults to 0
+ - `hidden` Allow transfer of hidden files. defaults to false
+ - `index` Default file name, defaults to 'index.html'
+ - `gzip`  Try to serve the gzipped version of a file automatically when gzip is supported by a client and if the requested file with .gz extension exists. defaults to true.
+
+## Example
+
+```js
+var serve = require('koa-static');
+var koa = require('koa');
+var app = koa();
+
+//GET /package.json
+app.use(serve('.'));
+
+// or use absolute paths
+app.use(serve(path.join(__dirname, ".")));
+
+// or use optx.prefix rewrite support
+// GET /static/package.json
+app.use(serve('.', {
+    prefix: "static"
+}));
+
+app.listen(3000);
+
+console.log('listening on port 3000');
+```
+
+### See also
+
+- [koajs/koa-send](https://github.com/koajs/send)
+- [koajs/koa-static](https://github.com/koajs/static)
---
+++ index.js
@@ -0,0 +1,65 @@
+'use strict';
+
+/**
+  Serves files from specified directory at specified path or from root.
+  Supports 'index' file.
+  @param {String} rootDir
+  @param {Object} opts
+  @return {Object} - {Function} serve
+  @api public
+*/
+
+const path = require("path");
+const assert = require("assert");
+const send = require("koa-send");
+
+module.exports = function(rootDir, opts) {
+
+    opts = opts || {};
+    assert(rootDir, 'rootDir is required to serve files');
+    assert.strictEqual(typeof rootDir, "string", "rootDir must be specified")
+
+    opts.root = path.resolve(rootDir);
+    opts.index = opts.index || "index.html";    
+
+
+    return function serve(ctx, next) {
+
+        return next().then(() => {
+
+            if (ctx.method != 'HEAD' && ctx.method != 'GET') return;
+            if (ctx.body != null || ctx.status != 404) return;
+
+            if (!opts.prefix) {
+                return send(ctx, ctx.path, opts);
+            } 
+
+            if (ctx.path.indexOf(opts.prefix) != 0) {
+                return;
+            }
+
+            /* Serve folders as specified
+             eg. :
+                rootDir = 'WEBAPP/public'
+                opts.prefix = '/static'
+            'WEBAPP/static/file.txt' will be served as 'http://server/static/file.txt'
+            */
+
+            // Redirect non-slashed request to slashed
+            // eg. /doc to /doc/
+
+            if (ctx.path == opts.prefix) {
+                return ctx.redirect(path.normalize(opts.prefix + "/"));
+            }
+
+            let prefixRegx = new RegExp("^" + opts.prefix);
+            if (opts.prefix) {
+                ctx.path = path.normalize(ctx.path.replace(prefixRegx, "/"));
+            }
+            return send(ctx, ctx.path, opts);
+
+        })
+        
+    }
+
+}
